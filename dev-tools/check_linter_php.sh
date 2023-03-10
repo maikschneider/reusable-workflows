e2db722267fe63f7c0f9e49538f2e32dc63bfa70
Maik Schneider <schneider.maik@me.com>
2023-03-10 08:42:37 +0100

    fix: php linter script

--- dev-tools/check_linter_php.sh
+++ dev-tools/check_linter_php.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find *.php . -name '*.php' ! -path './vendor/*'  ! -path './var/*' ! -path '*node_modules/*' -print0 | xargs -0 -n 1 -P 4 php -l
+find ./*.php . -name '*.php' ! -path './vendor/*'  ! -path './var/*' ! -path '*node_modules/*' ! -path './public/*' -print0 | xargs -0 -n 1 -P 4 php -l
